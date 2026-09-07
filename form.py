926a0ab71cedbfc6f66fe82e5191bcb12ecfd1e7
nicksutton99 <nicholas_sutton@hotmail.com>
2026-09-07 10:52:22 +0100

    Add Label
    Label in large font added

--- form.py
+++ form.py
@@ -1,9 +1,14 @@
 from tkinter import *
+from tkinter import font
+
 
 window = Tk()
-window.geometry("200x300")
+window.geometry("500x300")
+defaultFont = font.nametofont("TkDefaultFont")
+defaultFont.configure(family="Segoe Script", size=90, weight=font.BOLD)
 
 
-
+lab = Label(window, text="I'm Label")
+lab.pack()
 
 window.mainloop()
